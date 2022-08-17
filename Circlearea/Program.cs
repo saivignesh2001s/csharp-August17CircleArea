34c930651932f333672f5e219cd0fe5328d902f3
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-17 12:38:25 +0530

    Add project files.

---
+++ Circlearea/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Circlearea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float pi, radius;
+            pi = 3.14f;
+            radius = Convert.ToSingle(Console.ReadLine());
+            CircleArea(radius,pi);
+            Console.ReadLine();
+        }
+        static void CircleArea(float radius,float pi)
+        {
+            double area = pi * radius * radius;
+            Console.WriteLine(area);
+
+        }
+    }
+}
